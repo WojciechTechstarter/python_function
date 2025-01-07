c2fe618fdeb585a4a2ee0596fd7ea1dc4c2a02d7
WojciechTechstarter <wojciech.olakowski@tn.techstarter.de>
2025-01-07 22:09:15 +0100

    create class_ingredient

--- classes/class_ingredients.py
+++ classes/class_ingredients.py
@@ -1,5 +1,6 @@
 
 # creating a class ingredient
+
 class ingredient():
     # here is a contructor which shows the basic attributes of the class
     def __init__(self, name: str, calories: int, preperation_time: int):
@@ -8,28 +9,48 @@ class ingredient():
         self.preparation_time = preperation_time
 
         
-# Let's test it !
+# Testing the ingredient class
 
 potato = ingredient('potato', 77, 30)   # adding ingredients to the class (ingridients + attributes)
 
-# Cheking if works
 
 print(potato.name)                                          # potato
 print(potato.calories_pro_100g, potato.preparation_time)    # 77 30 
 
+# ---------------------------------
 
-
-# creating another class - recipe
+# creating another class - recipe with dictionary type inside
 
 class recipe():
     def __init__(self, name: str, description: str,):   # don't add type dictionary here
         self.name = name
         self.description = description
-# here's a dictionary first introduced (as a key, it doesn't have a str, but object ours ingridients list )
+# here's a dictionary first introduced (as a key, it doesn't have a str, but ?an object ours ingredient class? (line 4) )
         self.list_of_ingridients = {}      
 
+# ----------------------------------
 
-# Adding the method which adds ingridients to list of ingridients
+# adding a method that adds ingredient and its quantity to the recipe:
+    def add_ingredient(self, ingredient: ingredient, quantity: str):
+# adding the ingredient object as a key and the quantity as a value in the dictionary:
+        self.list_of_ingridients[ingredient] = quantity
 
-    def add_ingridient(ingredient):
-        
+# Testing the recipe class 
+# potato soup
+
+recipe_1 = recipe('Potato soup', 'A tasty soup mainly made out of potatoes')
+
+# Adding ingredients
+
+recipe_1.add_ingredient(potato, '300g')
+
+# Test
+print(recipe_1.name, recipe_1.description)
+# in order to print a whole dictionary we need a loop, that calls both the key and its value
+
+for ingredient, quantity in recipe.list_of_ingredients.items():
+    print()
+
+
+#  I stop here, seems a little complicated for now. I definetly got something out of it,
+#  but many questions appear and I did it mostly with the help of ChatGPT (which is fine, helped alot to understand some things).
--- classes/classes.py
+++ classes/classes.py
@@ -1,47 +1,40 @@
-
-
-class auto():
-    def __init__(self, name, motor, reichweite, kilometer):
+class Auto():
+    def __init__(self, name, motor, kilometer):
         self.name = name
         self.motor = motor
-        self.restreichweite = reichweite
         self.kilometerzahl = kilometer
-
-
-
 
     def fahren(self, km):
         if self.restreichweite >= km:
             self.kilometerzahl += km
             self.restreichweite -= km
-        else:
-            print(f'Reichweite nicht mehr ausreichend!')
+            print(f"Auto fährt {km}km.")
+        else: 
+            print("Reichweite nicht mehr ausreichend!")
 
-
-    def tanken(self, km):
-        self.restreichweite += km
-        
-  
-my_car = auto('Cintroen Xsara Picasso', 'Verbrenner', 200, 0)
-my_car.fahren(30)
-my_car.fahren(100)
-my_car.fahren(100)
-
-print(my_car.restreichweite)
-my_car.tanken(200)
-print(my_car.restreichweite)
-
-print(f'Kilometerstand: {my_car.kilometerzahl}')
-
-#  ---- Erbrende Klasse: E_auto
-class Verbrennner(auto):
+# ------ Erbende Klasse: Verbrenner
+# Über super().__init__(<args>) kann der Konstruktor der Superklasse aufgerufen werden
+class Verbrenner(Auto):
     def __init__(self, name, kilometer):
-        self.super(name, 'Verbrenner', kilometer)
+        super().__init__(name, "Verbrenner", kilometer)
         self.tankstand = 100
 
     def tanken(self, km):
-        self.tankstand +- km
+        self.tankstand += km
+        print("Vollgetankt mit Brennstoff")
 
-#  ---- Erbrende Klasse: E_auto
-class E_auto(auto):
-    def __init__
+# ----- Erbende Klasse: E_Auto
+class E_Auto(Auto):
+    def __init__(self, name, kilometer):
+        super().__init__(name, "Elektro", kilometer)
+        self.akkuladung = 100
+    
+    def tanken(self, km):
+        self.tankstand += km
+        print("Vollgetankt mit Ökostrom")
+
+my_e_car = E_Auto("Tesla Model X", 22210)
+my_combustion_car = Verbrenner("BMW m3", 13000)
+
+print(my_e_car.motor)
+print(my_combustion_car.motor)
